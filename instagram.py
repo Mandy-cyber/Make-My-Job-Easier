5f2b329a9b917888e1e612f7304570a30cbccf4d
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-24 15:49:51 -0500

    using cookies to stay logged in each time

--- instagram.py
+++ instagram.py
@@ -11,29 +11,13 @@ import pickle
 def set_up_page(accountName):
     options = Options()
     # options.add_argument("--headless")
-    options.add_argument("--log-level=3")
+    # options.add_argument("--log-level=3")
     options.add_experimental_option("detach", True)
+    options.add_argument("--user-data-dir=C:\\Users\\amand\\OneDrive\\Desktop\\Things For Work\\Make My Job Easier\\UserData")
+    options.page_load_strategy = 'normal'
     browser = webdriver.Chrome(options=options)
 
-    # pickle.dump( browser.get_cookies() , open("cookies.pkl","wb"))
-
     browser.get("https://www.instagram.com")
-    sign_in_username = browser.find_element_by_name("username")
-    sign_in_username.send_keys("")
-    sign_in_password = browser.find_element_by_name("password")
-    sign_in_password.send_keys("")
-    sign_in_password.send_keys(Keys.RETURN)
-
-    # save_details = browser.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/div/section/div/button""")
-    try:
-        element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((browser.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/div/section/div/button""")))
-    )
-    finally:
-        browser.quit()
-    # save_details.click()
-
-    # save_details = browser.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/div/section/div/button""")
 
     potentialAccounts = {}
     return browser
@@ -42,9 +26,3 @@ def set_up_page(accountName):
 
 accountName = "TVJ"
 browser = set_up_page(accountName)
-
-
-# def save_cookies(browser):
-#     cookies = pickle.load(open("cookies.pkl", "rb"))
-#for cookie in cookies:
-    # browser.add_cookie(cookie)
